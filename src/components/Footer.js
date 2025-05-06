e63d5320ec286cd6d7f800920f526d5b050dbb5a
huanghunz <huanghunz@hotmail.com>
2025-05-06 14:19:13 -0700

    footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,60 @@
 
 // Nav Component
+
+import Nav from './Nav';
+import './Footer.css';
+
+const footerImage = '../../icons_assets/restaurantfood.jpg';
+
+const contactInfo = [
+    {
+        "text": "123 Main St, Anytown, USA",
+        "elementId": "home-section"
+    },
+    {
+        "text": "info@littlelemon.com",
+        "elementId": "about-section"
+    },
+    {
+        "text": "123-456-7890",
+        "elementId": "menu-section"
+    },
+];
+
+const socialMedia = [
+    {
+        "text": "Facebook",
+        "externalLink": "https://www.facebook.com"
+    },
+    {
+        "text": "Twitter",
+        "externalLink": "https://www.twitter.com"
+    },
+    {
+        "text": "Instagram",
+        "externalLink": "https://www.instagram.com"
+    },
+];
+
 function Footer() {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className="footer">
+            <img className="footer-img" src={footerImage} alt="Little Lemon Restaurant" />
+            <div className="footer-content">
+                { /* 3 columns with list: nav, contact, social media */}
+                <div className="footer-column">
+                    <h3>Navigation</h3>
+                    <Nav className="footer-nav"/>
+                </div>
+                <div className="footer-column">
+                    <h3>Contact</h3>
+                    <Nav className="footer-nav" customLinksData={contactInfo}/>
+                </div>
+                <div className="footer-column">
+                    <h3>Social Media</h3>
+                    <Nav className="footer-nav" customLinksData={socialMedia}/>
+                </div>
+            </div>
         </footer>
     )
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,73 @@
+const defaultLinks = [
+    {
+        "text": "Home",
+        "elementId": "home-section"
+    },
+    {
+        "text": "About",
+        "elementId": "about-section"
+    },
+    {
+        "text": "Menu",
+        "elementId": "menu-section"
+    },
+    {
+        "text": "Reservation",
+        "elementId": "reservation-section"
+    },
+    {
+        "text": "Order Online",
+        "elementId": "order-online-section"
+    },
+    {
+        "text": "Login",
+        "elementId": "login-section"
+    },
+];
+
+function Nav(props) {
+
+    const {customLinksData} = props;
+    const navLinks = customLinksData ? customLinksData : defaultLinks;
+    const handleClick = (id) => () => {
+        /* Syntax note:
+            const handleClick = (anchor) => {
+            return () => {
+                console.log(anchor);
+            }
+            }
+            That returned function becomes the actual onClick handler.
+        */
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+    };
+    return (
+        <nav className="nav">
+            <div> {/* this div help sizing the logo in different screen sizes */}
+                <img className="nav-logo" src="/icons_assets/logo.svg" alt="Logo" />
+            </div>
+            {
+                navLinks.map((s) => {
+                    if (s.elementId !== undefined) {
+                        return (
+                            <a  className="nav-link"
+                                href={s.elementId} key={s.text} onClick={handleClick(s.elementId)}>{s.text}</a>
+                        )
+                    } else if (s.externalLink !== undefined) {
+                        return (
+                            <a  className="nav-link"
+                                href={s.externalLink} key={s.text} >{s.text}</a>
+                        )
+                    }
+                })
+            }
+        </nav>
+    )
+}
+
+export default Nav;
